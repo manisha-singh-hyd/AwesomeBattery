0720cba7dd794405e5c191029d269bd3d9b92664
Manisha <manisha.singh.hyd@gmail.com>
2025-06-01 17:16:23 +0530

    Initial Commit

---
+++ AwsomeBattery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AwsomeBattery
+//
+//  Created by Singh, Anand on 31/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
